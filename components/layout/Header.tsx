d03523d4fdb4d7b9e3aa9877eda9b01277ea5888
Denys-Dudnyk <83369962+Denys-Dudnyk@users.noreply.github.com>
2024-12-28 17:12:59 +0100

    dev popup

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 import { createClient } from '@/lib/supabase/client'
+import DevelopModal from '../ui/DevelopModal/DevelopModal'
 // import { useRouter } from 'next/router'
 
 const Header = () => {
@@ -19,6 +20,8 @@ const Header = () => {
 	const wePage = navigation === '/we' ? 'absolute' : ''
 	const mainPageHeader = navigation === '/' ? 'bg-header' : 'bg-[#0f0f0f]'
 	const wePageHeader = navigation === '/we' ? 'bg-header' : 'bg-[#0f0f0f]'
+
+	const [showDev, setShowDev] = useState(false)
 
 	useEffect(() => {
 		const checkAuth = async () => {
@@ -53,50 +56,61 @@ const Header = () => {
 		}
 	}
 
+	const openDevelopModal = () => {
+		setShowDev(true)
+	}
+
 	return (
-		<header className={`${mainPage} ${wePage} w-full z-[3]`}>
-			<div
-				className={`flex justify-between items-center px-20 py-[21px] ${mainPageHeader} ${wePageHeader}`}
-			>
-				<div>
-					<Link href={'/'}>
-						<Image
-							src={'/header/logo.svg'}
-							alt='Логотип Nomo'
-							width={82.5}
-							height={83.5}
-						/>
-					</Link>
-				</div>
-				<Navbar />
-				<div className='flex items-center gap-[10px]'>
-					{isAuthenticated ? (
-						<button
-							onClick={handleLogout}
-							className='bg-accent px-[30px] py-[10px] font-bold rounded-[40px] hover:bg-[#FFBF88] transition-colors'
-						>
-							Вихід
-						</button>
-					) : (
-						<Link
-							href={'/auth/login'}
-							className='bg-accent px-[30px] py-[10px] font-bold rounded-[40px] hover:bg-[#FFBF88] transition-colors'
-						>
-							Вхід
+		<>
+			<header className={`${mainPage} ${wePage} w-full z-[3]`}>
+				<div
+					className={`flex justify-between items-center px-20 py-[21px] ${mainPageHeader} ${wePageHeader}`}
+				>
+					<div>
+						<Link href={'/'}>
+							<Image
+								src={'/header/logo.svg'}
+								alt='Логотип Nomo'
+								width={82.5}
+								height={83.5}
+							/>
 						</Link>
-					)}
-					<button className='flex items-center gap-[11px] text-[24px] font-normal'>
-						<Image
-							src={'/header/language.svg'}
-							alt='Логотип Nomo'
-							width={42}
-							height={42}
-						/>
-						Українська
-					</button>
+					</div>
+					<Navbar />
+					<div className='flex items-center gap-[10px]'>
+						{isAuthenticated ? (
+							<button
+								onClick={handleLogout}
+								className='bg-accent px-[30px] py-[10px] font-bold rounded-[40px] hover:bg-[#FFBF88] transition-colors'
+							>
+								Вихід
+							</button>
+						) : (
+							<Link
+								href={'/auth/login'}
+								className='bg-accent px-[30px] py-[10px] font-bold rounded-[40px] hover:bg-[#FFBF88] transition-colors'
+							>
+								Вхід
+							</Link>
+						)}
+						<button
+							className='flex items-center gap-[11px] text-[24px] font-normal'
+							onClick={openDevelopModal}
+						>
+							<Image
+								src={'/header/language.svg'}
+								alt='Логотип Nomo'
+								width={42}
+								height={42}
+							/>
+							Українська
+						</button>
+					</div>
 				</div>
-			</div>
-		</header>
+			</header>
+
+			<DevelopModal isOpen={showDev} onClose={() => setShowDev(false)} />
+		</>
 	)
 }
 export default Header
